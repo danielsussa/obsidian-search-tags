93a493315cc8741d40b22fbebbe1cd2990c04e0f
Daniel Kanczuk <daniel.kanczuk@pismo.io>
2022-07-28 07:01:18 -0300

    refact raw data

--- main.ts
+++ main.ts
@@ -2,102 +2,7 @@ import { App, CachedMetadata, Editor, getAllTags, MarkdownView, MetadataCache, M
 
 // Remember to rename these classes and interfaces!
 
-
-class CachedStruct {
-	constructor() { 
-		this.fileMap = new Map<string, FileMap>();
-	 } 
-	fileMap: Map<string, FileMap>
-
-	setFileMap(file: TFile, cache: CachedMetadata | null, data: string) {
-		if (cache == null) {
-			this.fileMap.delete(file.name)
-			return 
-		}
-		
-		if (cache.tags == null && cache.frontmatter == null) {
-			this.fileMap.delete(file.name)
-			return
-		}
-
-		if (cache.sections == null) {
-			this.fileMap.delete(file.name)
-			return
-		}
-
-		let tagTexts: Array<TagText> = []
-		
-		
-		for (const section of cache.sections) {
-			
-			let tags: Array<TagCache> = []
-			// if (cache.frontmatter != null) {
-			// 	const tagsOpt = parseFrontMatterTags(cache.frontmatter)
-			// 	if (tagsOpt != null) {
-			// 		for (const tag of tagsOpt) {
-			// 			tags.push({
-			// 				tag: tag,
-			// 				position: {start: {line: 0, col: 0, offset: 0}, end: {line: 0, col: 0, offset: 0}} 
-			// 			})
-			// 		}
-			// 	}
-			// }
-			if (cache.tags != null) {
-				for (const tag of cache.tags) {
-					if (tag.position.start.offset >= section.position.start.offset && tag.position.end.offset <= section.position.end.offset) {
-						tags.push(tag)
-					}
-				}
-			}
-			if (tags.length > 0) {
-				tagTexts.push({
-					tags: tags,
-					text: data.substring(tags[0].position.start.offset - 100, tags[0].position.end.offset + 100),
-					cursor: tags[0].position.start.line
-				})
-			}
-		}
-
-		this.fileMap.set(file.name, {
-			path: file.path,
-			tagTexts: tagTexts,
-			file: file
-		})
-	}
-
-	toSelections() : Selection[] {
-		let selections: Array<Selection> = []
-
-		for(let key of this.fileMap.keys()) {
-			const fileMap = this.fileMap.get(key)
-			if (fileMap?.tagTexts == null) {
-				continue
-			}
-			for(let tagText of fileMap.tagTexts) {
-				selections.push({
-					title: fileMap.path,
-					description: tagText.text.replace(/[\r\n]/gm, '  '),
-					tags: tagText.tags.map(k => k.tag),
-					file: fileMap.file,
-					cursor: tagText.cursor
-				})
-			}
-		 }
-		return selections
-	}
-}
-
-class FileMap {
-	file: TFile;
-	path: string;
-	tagTexts: TagText[];
-}
-
-class TagText {
-	text: string
-	cursor: number
-	tags: TagCache[];
-}
+const regex1 = new RegExp(/(?<=[\s>]|^)#(\w*[A-Za-z_/]+\w*)/ig);
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -120,17 +25,9 @@ export default class MyPlugin extends Plugin {
 			id: 'open-modal',
 			name: 'Open Tag Selector',
 			checkCallback: (checking: boolean) => {
-				// const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				// if (markdownView) {
-				// 	// If checking is true, we're simply "checking" if the command can be run.
-
-				// }
-
-				// If checking is false, then we want to actually perform the operation.
 				if (!checking) {
 					new SelectorModal(this.app, cached.toSelections()).open();
 				}
-
 				// This command will only show up in Command Palette when the check function returns true
 				return true;
 			}
@@ -158,7 +55,6 @@ export default class MyPlugin extends Plugin {
 				cached.setFileMap(file, this.app.metadataCache.getFileCache(file), data)
 			});
 		})
-
 		return cached;
 	}
 
@@ -175,6 +71,64 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
+class CachedStruct {
+	fileMap: Map<string, Selection>
+	constructor() {
+		this.fileMap = new Map<string, Selection>
+	}
+
+	setFileMap(file: TFile, cache: CachedMetadata | null, data: string) {
+		let headerTags: Array<string> = []
+		if (cache?.frontmatter != null) {
+			parseFrontMatterTags(cache.frontmatter)?.forEach(t => headerTags.push(t))
+			this.fileMap.set(file.name, {
+				cursor: 0,
+				title: file.path,
+				description: data.replace(/[\r\n]/gm, '  ').substring(0,200),
+				tags: headerTags.sort(),
+				file: file
+			})
+		}
+
+		if (cache?.tags == null && headerTags.length == 0) {
+			this.fileMap.set(file.name, {
+				cursor: 0,
+				title: file.path,
+				description: "",
+				tags: [],
+				file: file
+			})
+			return 
+		}
+
+		const dataSpl = data.split("\n")
+		let offset = 0;
+		for (let i = 0 ; i < dataSpl.length ; i++){
+			const paragraph = dataSpl[i]
+			const tagIdx = paragraph.search(regex1)
+	
+			if (tagIdx != -1) {
+				const tags = [...paragraph.matchAll(regex1)].map(k => k[0]);
+				tags.push(...headerTags)
+				this.fileMap.set(file.name, {
+					cursor: i,
+					title: file.path,
+					description: data.replace(/[\r\n]/gm, '  ').substring(offset+tagIdx-100, offset+tagIdx+100),
+					tags: tags.sort(),
+					file: file
+				})
+			}
+			offset += paragraph.length
+		}
+	}
+
+	toSelections() : Selection[] {
+		let selections: Array<Selection> = []
+		this.fileMap.forEach(k => selections.push(k))
+		return selections
+	}
+}
+
 interface Selection {
 	cursor: number;
 	file: TFile;
@@ -188,43 +142,40 @@ class SelectorModal extends SuggestModal<Selection> {
 	allResults: Selection[];
 
 	getSuggestions(query: string): Selection[] | Promise<Selection[]> {
-		const querySpl = query.split(" ")
+		const getOrphan = query.startsWith("!")
 		return this.allResults.filter((page) => {
-			for (let i = 0; i < querySpl.length; i++) {
-				const subQuery = querySpl[i]
-				const tagsJoin = page.tags.join() // ["fdsfs", "ffgg"]
-				if (!tagsJoin.contains(subQuery)) {
+			if (getOrphan) {
+				return page.tags.length == 0;
+			} else {
+				if (page.tags.length == 0) {
 					return false
 				}
+				for (const subQuery of query.split(" ")) {
+					
+					const tagsJoin = page.tags.join()
+					if (!tagsJoin.contains(subQuery)) {
+						return false
+					}
+				}
+				return true
 			}
-			return true
+
 		});
 	}
-	// renderSuggestion(value: Selection, el: HTMLElement) {
-
-	// 	const result1 = el.createEl("div",{ cls: "suggestion-item mod-complex qsp-suggestion-headings qsp-headings-l1"})
-	// 	const result1Content = result1.createEl("div",{ cls: "suggestion-content qsp-content"});
-	// 	// title
-	// 	result1Content.createEl("div",{ cls: "suggestion-title qsp-title", text: value.title})
-	// 	// sugestion note
-	// 	result1Content.createEl("div",{ cls: "suggestion-note qsp-note"}).createEl("span",{ cls:"qsp-path", text: value.description})
-	// 	// right element
-	// 	result1.createEl("div", {cls:"suggestion-aux qsp-aux"}).createEl("span",{cls:"suggestion-flair qsp-headings-indicator", text:"H₁"})
-		
-	// 	const tagContainer = el.createEl("div").createEl("p")
-	// 	for (let i = 0; i < value.tags.length; i++) {
-	// 		tagContainer.createEl("a", { text: value.tags[i], cls: "tag" });
-	// 	}
-	// }
 
 	renderSuggestion(value: Selection, el: HTMLElement) {
-		const selection = el.createEl("div", { cls: "selection" });
-		selection.createEl("div", { text: value.title, cls: "selection__title" });
-		selection.createEl("small", { text: value.description, cls: "selection__description" });
-
-		const tagContainer = selection.createEl("p")
-		for (let i = 0; i < value.tags.length; i++) {
-			tagContainer.createEl("a", { text: value.tags[i], cls: "tag selection__tag" });
+		if (value.tags.length > 0) {
+			const selection = el.createEl("div", { cls: "selection" });
+			selection.createEl("div", { text: value.title, cls: "selection__title" });
+			selection.createEl("small", { text: value.description, cls: "selection__description" });
+	
+			const tagContainer = selection.createEl("p")
+			for (let i = 0; i < value.tags.length; i++) {
+				tagContainer.createEl("a", { text: value.tags[i], cls: "tag selection__tag" });
+			}
+		}else {
+			const selection = el.createEl("div", { cls: "selection" });
+			selection.createEl("div", { text: value.title, cls: "selection__title" });
 		}
 	}
 
